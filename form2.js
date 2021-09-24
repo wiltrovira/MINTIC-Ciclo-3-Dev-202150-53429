392c86cbc21eb0fbe12a48bb9d7f0faefd468cb7
Wilt Rovira <wrovira@gmail.com>
2021-09-24 18:52:59 -0500

    funcion validar_correo implementada

--- form2.js
+++ form2.js
@@ -5,13 +5,26 @@ const usuario = /[A-Z]/
 const pass = /[A-Z]/
 const email = /\w+@\w\.+[a-z]/
 
+/**
+ * validar_correo
+ * @param {*} correo 
+ * @returns 
+ */
 function validar_correo(correo) {
-    correo = email.test(correo.value) && correo.value.length > 15 && correo.value.length < 40
-    if (correo)
-        return true
-    return false
+    correo = email.test(correo.value) && correo.value.length > 15 && correo.value.length < 40;
+    expresionregular = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (expresionregular.test(correo.value)){
+        return (true);
+    }
+    return (false);
 }
 
+/**
+ * validar_contrasena_usuario
+ * @param {*} user 
+ * @param {*} password 
+ * @returns 
+ */
 function validar_contrasena_usuario(user, password) {
     user = usuario.test(user.value) && user.value.length > 5 && user.value.length < 12
     password = pass.test(password.value) && password.value.length > 5 && password.value.length < 12
