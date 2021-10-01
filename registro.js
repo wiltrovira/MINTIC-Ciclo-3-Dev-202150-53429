88a3f390b74b36cd9cf40aa07d430a14699b1de9
zamir torres <wztorresc@unipacifico.edu.co>
2021-10-01 17:00:42 -0500

    funcion ordenamiento

--- registro.js
+++ registro.js
@@ -1,11 +1,11 @@
 var DatosRegistro = [];
 
-function almacenarRegistro(){
+function almacenarRegistro() {
     var username = document.getElementById("campoNombre").value;
     var password = document.getElementById("campoContraseña").value;
     var email = document.getElementById("campoCorreo").value;
     var telefono = document.getElementById("campoTelefonoMovil").value;
-    
+
     var nuevoRegistro = {
         nombre: username.value,
         contraseña: password.value,
@@ -13,12 +13,24 @@ function almacenarRegistro(){
         telefonoMovil: telefono.value,
     };
 
-    console.log(nuevoRegistro); 
+    console.log(nuevoRegistro);
     DatosRegistro.push(nuevoRegistro);
 }
 
+function ordenarRegistros(DatosRegistro) {
+
+    DatosRegistro.sort((a, b) => {
+
+        if (a.nombre.toLowelcase > b.nombre.toLowerCase)
+            return DatosRegistro
+        if (a.nombre < b.nombre)
+            return DatosRegistro
+
+        return DatosRegistro
+
+    });
+}
 
 
-
- /*   myForm.addEventListener("submit",function (event){
-        event.preventDefault */
+/*   myForm.addEventListener("submit",function (event){
+       event.preventDefault */
