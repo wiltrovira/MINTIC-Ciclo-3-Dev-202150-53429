f97ee9836e626623ad4fbd6f4edf8726e75862af
Wilt Rovira <wrovira@gmail.com>
2021-10-01 19:56:22 -0500

    registro.js para almacenarRegistros()

--- registro.js
+++ registro.js
@@ -1,24 +1,51 @@
-var DatosRegistro = [];
+// console.log("ejecutando registros");
 
-function almacenarRegistro(){
-    var username = document.getElementById("campoNombre").value;
-    var password = document.getElementById("campoContraseña").value;
-    var email = document.getElementById("campoCorreo").value;
-    var telefono = document.getElementById("campoTelefonoMovil").value;
+var registros = [];
+
+var myForm = document.getElementById("formulario-registro");
+
+var usuario = document.getElementById("campoNombre");
+var contrasena = document.getElementById("campoContraseña");
+var correo = document.getElementById("campoCorreo");
+var telefono = document.getElementById("campoTelefonoMovil");
+
+
+myForm.addEventListener("submit", function(event){
+    event.preventDefault();
     
+    // console.log("clic presionado");
+    almacenarRegistros();
+});
+
+
+//Función para almacenar registros
+function almacenarRegistros(){
+    // console.log("entro a almacenarRegistros()...");
+
     var nuevoRegistro = {
-        nombre: username.value,
-        contraseña: password.value,
-        correo: email.value,
-        telefonoMovil: telefono.value,
+        "campoNombre": usuario.value,
+        "campoContraseña": contrasena.value,
+        "campoCorreo": correo.value,
+        "campoTelefonoMovil": telefono.value
     };
 
-    console.log(nuevoRegistro); 
-    DatosRegistro.push(nuevoRegistro);
+    if (registros.length <30){
+        registros.push(nuevoRegistro);
+        return true;
+    }else{
+        return false;
+    } 
 }
 
 
+// Función para ordenar registros
+function ordenarRegistros(args){
 
 
- /*   myForm.addEventListener("submit",function (event){
-        event.preventDefault */
+}
+
+// almacenarRegistros();
+
+module.exports.registros = registros;
+module.exports.almacenarRegistros = almacenarRegistros();
+module.exports.ordenarRegistros = ordenarRegistros();
